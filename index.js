c1f3be1e3c71ce2edd067bff2c24876adba89b26
Enric Abad <enricabadrovira@gmail.com>
2025-09-15 12:03:22 +0200

    trigger deploy

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.log(`Servidor escoltant al port ${PORT}`)
+  console.log(`Servidor escoltant al port num ${PORT}`)
 })
